1c8664896cf12c3885d806437acfe7eff156ce5d
Boris Jokic <32968807+rerih@users.noreply.github.com>
2019-10-28 11:42:52 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-This attachment was removed.
+const mobMenu = document.querySelector('.mobMenu');
+const mobMenuList = document.querySelector("nav ul");
+
+
+  mobMenu.addEventListener("click", myFunc);
+
+
+function myFunc() {
+  console.log("clicked");
+  mobMenuList.classList.toggle("mobMenuList-active");
+}
+
